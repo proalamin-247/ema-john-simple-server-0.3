9553db7e898606b639ddfa07102017bb033a2675
proalamin-247 <pro.alamin247@gmail.com>
2022-04-30 00:01:42 +0600

    product count api added

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ async function run(){
             const products= await cursor.toArray();
             res.send(products);
         })
+
+        app.get('/productCount', async(req, res)=>{
+            const query ={};
+            const cursor = productCollection.find(query);
+            const count = await cursor.count();
+            res.send({count});
+        })
+
     }
     finally{
 
