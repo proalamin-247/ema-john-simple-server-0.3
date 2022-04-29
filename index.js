88248d12abebbc5d1b40797ccb64778e5b24a4f2
proalamin-247 <pro.alamin247@gmail.com>
2022-04-29 20:04:14 +0600

    node and express server ready and monogDB user create and almot connect successfull

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oinvi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+    }
+    finally{
+        
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) =>{
+    res.send('Ema jon server is running');
+});
+
+
+app.listen(port, () =>{
+    console.log('ema john server is runing', port);
+})
